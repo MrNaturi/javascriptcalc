6f7027d5b5466803b9b47f50819beb869f5c36fa
Emmanuel <daniaemmanuel06@gmail.com>
2024-11-26 15:18:24 +0100

    overflow

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Display = ({ display }) => {
     return (
       <div 
-        className="bg-white text-black py-4 px-6 text-4xl rounded-lg w-full h-20 flex items-center justify-end overflow-hidden"
+        className="bg-white text-black py-4 px-6 text-4xl rounded-lg w-full h-20 flex items-center justify-end"
       >
         {display}
       </div>
